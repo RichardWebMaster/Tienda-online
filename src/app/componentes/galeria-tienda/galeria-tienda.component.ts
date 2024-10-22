ed83a5cdbc8acfda5f715f00d6660a0d5ff04f8e
https://github.com/RichardWebMaster <richardtorrespinzon@gmail.com>
2024-10-22 15:32:03 -0500

    contenido pagina galeria

--- src/app/componentes/galeria-tienda/galeria-tienda.component.ts
+++ src/app/componentes/galeria-tienda/galeria-tienda.component.ts
@@ -9,4 +9,78 @@ import { Component } from '@angular/core';
 })
 export class GaleriaTiendaComponent {
 
+productos=[
+  {
+
+    name:"camisa de verano",
+    size: "M",
+    stock: "100 Uunidades",
+    price: "200000",
+    category: "sacos",
+    brand: "Nike",
+    sku: "saco-pluma-nike",
+    picture: "https://armatura.com.co/cdn/shop/files/camisa-hombre-cuadros-azul-naval-cuello.webp?v=1716839126&width=540"
+
+
+},
+
+{
+  name:"camisa de invierno",
+  size: "S",
+  stock: "200 unidades",
+  price: "300000",
+  category: "camisas",
+  brand: "Adidas",
+  sku: "camisa-nike",
+  picture: "https://img.kwcdn.com/product/fancy/2b5091d0-fd22-4f0c-9325-59a45b8c0995.jpg?imageView2/2/w/800/q/70/format/webp"
+
+
+},
+{
+  name:"camisa de invierno",
+  size: "L",
+  stock: "200 unidades",
+  price: "300000",
+  category: "camisa",
+  brand: "Adidas",
+  sku: "camisa-nike",
+  picture: "https://armatura.com.co/cdn/shop/files/camisa-hombre-cuadros-azul-naval-cuello.webp?v=1716839126&width=540"
+
+},
+{
+  name:"camisa de invierno",
+  size: "L",
+  stock: "200 unidades",
+  price: "300000",
+  category: "camisa",
+  brand: "Adidas",
+  sku: "camisa-nike",
+  picture: "https://armatura.com.co/cdn/shop/files/camisa-hombre-cuadros-azul-naval-cuello.webp?v=1716839126&width=540"
+
+},
+{
+  name:"camisa de invierno",
+  size: "L",
+  stock: "200 unidades",
+  price: "300000",
+  category: "camisa",
+  brand: "Adidas",
+  sku: "camisa-nike",
+  picture: "https://armatura.com.co/cdn/shop/files/camisa-hombre-cuadros-azul-naval-cuello.webp?v=1716839126&width=540"
+
+},
+{
+  name:"camisa de invierno",
+  size: "L",
+  stock: "200 unidades",
+  price: "300000",
+  category: "camisa",
+  brand: "Adidas",
+  sku: "camisa-nike",
+  picture: "https://armatura.com.co/cdn/shop/files/camisa-hombre-cuadros-azul-naval-cuello.webp?v=1716839126&width=540"
+
+}
+
+]
+
 }
--- src/app/paginas/tienda/tienda.component.ts
+++ src/app/paginas/tienda/tienda.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { NavbarComponent } from '../../componentes/navbar/navbar.component';
+import { GaleriaTiendaComponent } from '../../componentes/galeria-tienda/galeria-tienda.component';
+import { FooterComponent } from '../../componentes/footer/footer.component';
 
 @Component({
   selector: 'app-tienda',
   standalone: true,
-  imports: [],
+  imports: [NavbarComponent,GaleriaTiendaComponent,FooterComponent],
   templateUrl: './tienda.component.html',
   styleUrl: './tienda.component.css'
 })
